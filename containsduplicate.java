b254738f88a453f6fd3d62b552be35f69ca49a45
Priyal Khurana <78503249+priyalkhurana@users.noreply.github.com>
2023-06-03 14:00:39 +0530

    Update containsduplicate.java

--- containsduplicate.java
+++ containsduplicate.java
@@ -12,3 +12,19 @@ class Solution {
         return false;
     }
 }
+
+
+
+better:
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            
+               if(nums[i]==nums[i+1]){
+                return true;
+                }
+            }   
+        return false;
+    }
+}
